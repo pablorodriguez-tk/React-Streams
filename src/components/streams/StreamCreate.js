929cadd961ec084026a703f9a7bed7345df19246
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-03-26 20:53:34 -0300

    Add Bulk action creators

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
@@ -23,9 +25,9 @@ class StreamCreate extends React.Component {
     );
   };
 
-  onSubmit(formValues) {
-    console.log(formValues);
-  }
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues);
+  };
 
   render() {
     return (
@@ -58,7 +60,9 @@ const validate = (formValues) => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: "steamCreate", // a unique identifier for this form
   validate, // <--- validation function given to redux-form
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
