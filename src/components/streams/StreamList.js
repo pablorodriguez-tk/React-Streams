e9c8379552ea69ad3f0499dec898b8f823ae1818
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-03-27 04:22:54 -0300

    Add Edit and Create

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -12,7 +12,9 @@ class StreamList extends React.Component {
     if (stream.userId === this.props.currentUserId) {
       return (
         <div className="right floated content">
-          <button className="ui button primary">Edit</button>
+          <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
+            Edit
+          </Link>
           <button className="ui button negative">Delete</button>
         </div>
       );
@@ -39,7 +41,7 @@ class StreamList extends React.Component {
       return (
         <div style={{ textAlign: "right" }}>
           <Link to="/streams/new" className="ui button primary">
-            create Stream
+            Create Stream
           </Link>
         </div>
       );
